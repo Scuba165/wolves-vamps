bab3921b87de52505bef3db491f4b4fe3fe8d399
Scuba165 <chr.pap02@gmail.com>
2022-11-29 11:52:41 +0200

    a

--- Code/include/board.cpp
+++ Code/include/board.cpp
@@ -2,42 +2,42 @@
 #include "tile.cpp" // thelw na kanw include to h alla den to exw vrei
 using namespace std;
 
-tile** create_board(int size) {
-    tile** board = new tile*[size];
-    for(int i = 0; i < size; i++ ) {
-        board[i] = new tile[size];
+tile** create_board(int size_x, int size_y) {
+    tile** board = new tile*[size_x];
+    for(int i = 0; i < size_x; i++ ) {
+        board[i] = new tile[size_y];
     }
     return board;
 }
 // Adds trees, lakes, potion.
-void add_objects(tile* board[], int size) {
+void add_objects(tile* board[], int size_x, int size_y) {
     // Trees
-    for(int i = 0; i < (size*size / 15) + 1; i++) {
-        int x = rand() % size;
-        int y = rand() % size;
+    for(int i = 0; i < (size_x*size_y / 15) + 1; i++) {
+        int x = rand() % size_x;
+        int y = rand() % size_y;
         board[x][y].is_tree = true;
         board[x][y].is_occupied = true;
     }
     // Lakes
-    for(int i = 0; i < (size*size / 20) + 1; i++) {
-        int x = rand() % size;
-        int y = rand() % size;
+    for(int i = 0; i < (size_x*size_y / 20) + 1; i++) {
+        int x = rand() % size_x;
+        int y = rand() % size_y;
         board[x][y].is_lake = true;
         board[x][y].is_occupied = true;
     }
     // Potion
-    int x = rand() % size;
-    int y = rand() % size;
+    int x = rand() % size_x;
+    int y = rand() % size_y;
     while(board[x][y].is_occupied) {
-        int x = rand() % size;
-        int y = rand() % size;
+        int x = rand() % size_x;
+        int y = rand() % size_y;
     }
     board[x][y].is_potion = true;
 }
 
-void print_board(tile* board[], int size) {
-    for(int i = 0; i < size; i++) {
-        for(int j = 0; j < size; j++) {
+void print_board(tile* board[], int size_x, int size_y) {
+    for(int i = 0; i < size_x; i++) {
+        for(int j = 0; j < size_y; j++) {
             cout << board[i][j].print() << " ";  
         }
         cout << endl;
